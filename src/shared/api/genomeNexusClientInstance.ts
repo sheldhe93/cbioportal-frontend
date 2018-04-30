c92700bb875a84165bc78b671d0677d4f3040be2
Ino de Bruijn <ino@ino.pm>
2018-04-30 09:17:34 -0400

    temp use heroku genome nexus

--- src/shared/api/genomeNexusClientInstance.ts
+++ src/shared/api/genomeNexusClientInstance.ts
@@ -8,7 +8,7 @@ async function checkVersion(client:GenomeNexusAPI) {
     }
 }
 
-const client = new GenomeNexusAPI(getGenomeNexusApiUrl());
+const client = new GenomeNexusAPI("https://genomenexus.herokuapp.com");
 checkVersion(client);
 
 export default client;
