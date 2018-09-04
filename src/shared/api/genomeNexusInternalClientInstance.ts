4393358487d1aa52ad6693a852d3842b4c2f8833
Ino de Bruijn <ino@ino.pm>
2018-09-04 08:16:41 -0400

    use heroku for gn

--- src/shared/api/genomeNexusInternalClientInstance.ts
+++ src/shared/api/genomeNexusInternalClientInstance.ts
@@ -1,6 +1,6 @@
 import {getGenomeNexusApiUrl} from "./urls";
 import GenomeNexusAPIInternal from "./generated/GenomeNexusAPIInternal";
 
-const client = new GenomeNexusAPIInternal(getGenomeNexusApiUrl());
+const client = new GenomeNexusAPIInternal("https://genomenexus.herokuapp.com");
 
 export default client;
